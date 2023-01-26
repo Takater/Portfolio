d233cfd30b6982b8dfcc7bd5240578677ffd033c
Guilherme <guilherme.moret.ps@gmail.com>
2023-01-26 18:20:15 -0400

    Updating Commit 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import dev from '../images/dev.png'
-import gui from '../images/gui.png'
-import Clouds from './Clouds'
 function Header () {
 
     return (
         <header id="headerTag">
             <div className="header-div">
-                <Clouds />
+                { /*<Clouds /> */}
                 <h1><span> Guilherme Moret </span></h1>
-                <img className="profilepic" src={gui} alt="Personal" />
                 <h3><em> Software Engineering Student and Full Stack Developer I </em></h3>
             </div>
-                <img className="dev" src={dev} alt="Developer Desk" />
         </header>
     );
 }
